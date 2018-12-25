c0e99db988afae745f2054ced4a9044527ec0394
Hongkuan Wang <hongkuan.wang@aalto.fi>
2018-12-25 16:19:22 +0200

    Fix bug in lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,6 +83,7 @@ public class IntList {
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
 
+        IntList L;
         /* Check A is null or not */
         if (A != null){
             L = A;
