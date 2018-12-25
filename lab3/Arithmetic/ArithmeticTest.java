86d1658c70fc4304846c10fb1fea97fa586ec1ac
Hongkuan Wang <hongkuan.wang@aalto.fi>
2018-12-25 17:58:22 +0200

    Solved lab3

--- lab3/Arithmetic/ArithmeticTest.java
+++ lab3/Arithmetic/ArithmeticTest.java
@@ -30,6 +30,6 @@ public class ArithmeticTest {
 
     /* Run the unit tests in this file. */
     public static void main(String... args) {        
-        jh61b.junit.TestRunner.runTests("all", ArithmeticTest.class);
+        jh61b.junit.TestRunner.runTests("failed", ArithmeticTest.class);
     }
 }
--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -70,4 +70,14 @@ public class IntListTest {
       * to add a main method. See ArithmeticTest.java for an
       * example. */
 
+    @Test
+    public void testReverse() {
+        IntList A = IntList.of(1, 2, 3);
+        IntList B = null;
+        IntList C = IntList.of(4, 5, 6);
+        IntList exp = IntList.reverse(C);
+        assertEquals(IntList.of(3, 2, 1), IntList.reverse(A));
+        assertEquals(null, IntList.reverse(B));
+        assertNotEquals(exp, C);
+    }
 }
