b0bbd57d66d5456f81574e58f80cde19f274e65b
Hongkuan Wang <hongkuan.wang@aalto.fi>
2018-12-25 23:08:33 +0200

    Fix uppercase bug for proj1b

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -8,7 +8,7 @@ public class OffByN implements CharacterComparator {
 
     @Override
     public boolean equalChars(char x, char y) {
-        if (x - y == n || y - x == n) {
+        if (Character.toLowerCase(x) - Character.toLowerCase(y) == n || Character.toLowerCase(y) - Character.toLowerCase(x) == n) {
             return true;
         }
         return false;
--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -2,7 +2,7 @@ public class OffByOne implements CharacterComparator {
 
     @Override
     public boolean equalChars(char x, char y) {
-        if (x - y == 1 || y - x == 1) {
+        if (Character.toLowerCase(x) - Character.toLowerCase(y) == 1 || Character.toLowerCase(y) - Character.toLowerCase(x) == 1) {
             return true;
         }
         return false;
